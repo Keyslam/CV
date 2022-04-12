6f708352b55aa3c5c173db832fe3cf4d00c67e5d
Tjakka5 <justinvanderleij@gmail.com>
2022-04-12 18:36:35 +0200

    Update info

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 
 type language = "nl" | "en";
 
-const activeLanguage: language = "en";
+const activeLanguage: language = "nl";
 
 type localizedStringGlobal = {
 	global: string,
@@ -274,8 +274,8 @@ function skillItem(doc: PDFKit.PDFDocument, skillItem: SkillItem) {
 
 function skills(doc: PDFKit.PDFDocument, skillItems: Array<SkillItem>) {
 	header(doc, {
-		nl: "Vaardigheden",
-		en: "Skills"
+		nl: "Vaardigheden & Kwaliteiten",
+		en: "Skills & Qualities"
 	});
 
 	skillItems.forEach(item => {
@@ -292,7 +292,7 @@ pageHeader(doc);
 
 personalia(doc, [
 	{subject: {nl: "Naam", en: "Name"}, content: {global: "Justin van der Leij"}},
-	{subject: {nl: "Adres", en: "Address"}, content: {global: "Wismastate 82, Leeuwarden"}},
+	{subject: {nl: "Adres", en: "Address"}, content: {global: "Lieuwenburg 45, Leeuwarden"}},
 	{subject: {nl: "Telefoonnummer", en: "Phone number"}, content: {global: "+31-611032664"}},
 	{subject: {nl: "E-mailadres", en: "E-mailaddress"}, content: {global: "justinvanderleij@gmail.com"}},
 	{subject: {nl: "Geboortedatum", en: "Date of birth"}, content: {global: "11-08-1998"}},
@@ -304,7 +304,7 @@ personalia(doc, [
 
 profile(doc, {nl: `Ik ben een gedreven en enthousiaste software ontwikkelaar met een passie voor game development.
 Geef mij een lastig probleem en ik zal met plezier uitpuzzelen hoe ik het kan oplossen.
-Ik sta altijd open om nieuwe dingen te leren en ben dus altijd opzoek naar een nieuwe uitdaging.
+Ik sta altijd open om nieuwe dingen te leren en ben daarom dus altijd op zoek naar een nieuwe uitdaging.
 `, en: `I am a motivated and enthusiastic software engineer with a passion for game development.
 Give me a difficult problem and I will happily figure out a solution.
 I'm always open to try new things and am always looking for a new challenge.`});
@@ -319,14 +319,14 @@ educations(doc, [
 
 	{
 		name: {nl: "MBO-4 Applicatie Ontwikkeling", en: "MBO-4 Application Development"}, 
-		date: {global: "Sept. 2014 – 2019"}, 
+		date: {global: "Sept. 2014 - 2019"}, 
 		location: {global: "Friesland College, Leeuwarden"}, 
 		extra: {nl: "Diploma behaald", en: "Diploma achieved"},
 	},
 
 	{
 		name: {global: "VMBO-TL"}, 
-		date: {global: "Sept. 2010 – 2014"}, 
+		date: {global: "Sept. 2010 - 2014"}, 
 		location: {global: "Dr. J. de Graafschool, Groningen"}, 
 		extra: {nl: "Diploma behaald", en: "Diploma achieved"},
 	},
@@ -334,56 +334,68 @@ educations(doc, [
 
 workExperience(doc, [
 	{
+		name: {nl: "Student-assistent / Docent Game Development", en: "Student-assistent / Teacher Game Development"}, 
+		date: {nl: "Feb. 2022 - Heden", en: "Feb. 2022 - Now"}, 
+		location: {global: "MBO Friese Poort, Leeuwarden"},
+	},
+
+	{
+		name: {nl: "Stagair Software Engineer", en: "Intern Software Engineer"}, 
+		date: {nl: "Sep. 2021 - Jan. 2022", en: "Sep. 2021 - Jan. 2022"}, 
+		location: {global: "ISA-Lab, Leeuwarden"},
+	},
+
+	{
 		name: {nl: "Student-assistent", en: "Student-assistent"}, 
-		date: {nl: "Nov. 2020 – Mrt. 2021", en: "Nov. 2020 - Mar. 2021"}, 
+		date: {nl: "Nov. 2020 - Mrt. 2021", en: "Nov. 2020 - Mar. 2021"}, 
 		location: {global: "NHL Stenden, Leeuwarden"},
 	},
 
 	{
 		name: {nl:"Begeleider Tech workshops", en: "Host Tech workshops"}, 
-		date: {global:"Jun. 2020 – Nov. 2020"}, 
+		date: {global:"Jun. 2020 - Nov. 2020"}, 
 		location: {global:"BNFRL, Friesland"},
 	},
 
 	{
 		name: {nl: "Full stack Web Developer", en: "Full stack Web Developer"}, 
-		date: {global: "Feb. 2020 – Dec. 2020"}, 
+		date: {global: "Feb. 2020 - Dec. 2020"}, 
 		location: {global: "FVB, Utrecht"},
 	},
 
 	{
 		name: {nl: "Deeltijd docent Web Development", en: "Parttime teacher Web Development"}, 
-		date: {nl: "Okt. 2019 – Nov. 2019", en: "Oct. 2019 – Nov. 2019"}, 
+		date: {nl: "Okt. 2019 - Nov. 2019", en: "Oct. 2019 - Nov. 2019"}, 
 		location: {global: "NHL Stenden, Leeuwarden"},
 	},
 
 	{
 		name: {nl: "Student-assistent C#", en: "Student-assistent C#"}, 
-		date: {global: "Sept. 2019 – Feb. 2020"}, 
+		date: {global: "Sept. 2019 - Feb. 2020"}, 
 		location: {global: "NHL Stenden, Leeuwarden"},
 	},
 
 	{
 		name: {nl: "Stagiair Game Developer", en: "Intern Game Developer"}, 
-		date: {global: "Sept. 2018 – Feb. 2019"}, 
+		date: {global: "Sept. 2018 - Feb. 2019"}, 
 		location: {global: "Grendel Games, Leeuwarden"},
 	},
 
 	{
 		name: {nl: "Vuilnisman", en: "Garbage man"}, 
-		date: {global: "Aug. 2016 – Dec. 2016"}, 
+		date: {global: "Aug. 2016 - Dec. 2016"}, 
 		location: {global: "Omrin, Leeuwarden"},
 	},
 
 	{
 		name: {nl: "AGF Medewerker", en: "Fresh produce employee"}, 
-		date: {nl: "Mrt. 2015 – Sept. 2015", en: "Mar. 2015 - Sept. 2015"}, 
+		date: {nl: "Mrt. 2015 - Sept. 2015", en: "Mar. 2015 - Sept. 2015"}, 
 		location: {global: "Jumbo, Leeuwarden"},
 	},
 
 	{
 		name: {nl: "Stagiair Front-end Web Development", en: "Intern Front-end Web Development"}, 
-		date: {nl: "Jan. 2015 – Mei 2015", en: "Jan. 2015 – May 2015"}, 
+		date: {nl: "Jan. 2015 - Mei 2015", en: "Jan. 2015 - May 2015"}, 
 		location: {global: "FCSprint, Leeuwarden"},
 	},
 ])
